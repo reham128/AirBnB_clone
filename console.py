97f035d6fc984e061788313616acb2a2a56c7026
Reham <r_nour30@yahoo.com>
2024-02-07 14:21:33 -0800

    temporary

--- console.py
+++ console.py
@@ -2,6 +2,9 @@
 """console module that represent the entry point to the app"""
 import cmd
 
+app_classes = {"BaseModel": BaseModel, "User": User, "State": State,
+        "Amenity": Amenity, "Place": Place, "City": City, "Review": Review}
+
 
 class HBNBCommand(cmd.Cmd):
     """class to handel the command line interpretr to act like the
@@ -14,7 +17,7 @@ class HBNBCommand(cmd.Cmd):
         return (True)
 
     def do_quit(self, line):
-        """to quit the program"""
+        """Quit command to exit the program"""
         return (True)
 
     def emptyline(self):
@@ -23,20 +26,92 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, line):
         """to create new instance and save it to json file"""
-        pass
+        arg = line.split()
+        if not arg:
+            print("** class name missing **")
+        elif arg[0] not in app_classes:
+            print("** class doesn't exist **")
+        else:
+            new_obj = app_classes[arg[0]]()
+            print(new_obj.id)
+            new_obj.save()
 
     def do_show(self, line):
-        """Prints the string representation of an instance"""
-        pass
+        """Prints the string representation of an instance
+        based on the class name and id"""
+        arg = line.split()
+        if not line:
+            print("** class name missing **")
+        elif arg[0] not in app_classes.keys():
+            print("** class doesn't exist **")
+        elif len(arg) < 2:
+            print("** instance id missing **")
+        else:
+            new_obj = "{}.{}".format(arg[0], arg[1])
+            dic = models.storage.all()
+            if new_obj not in dic:
+                print("** no instance found **")
+            else:
+                print(dic[new_obj])
 
     def do_destroy(self, line):
         """Deletes an instance based on the class name and id
         and save the changes to the json file"""
-        pass
+        arg = line.split()
+        if not arg:
+            print("** class name missing **")
+            return False
+        elif arg[0] not in app_classes:
+            print("** class doesn't exist **")
+        elif len(arg) < 2:
+            print("** instance id missing **")
+        else:
+            new_obj = "{}.{}".format(arg[0], arg[1])
+            if new_obj not in models.storage.all():
+                print("** no instance found **")
+            else:
+                del models.storage.all()[new_obj]
+                models.storage.save()
 
     def do_all(self, line):
-        """to print all instances of based or not on the class name"""
-        pass
+        """Prints all string representation of all instances based
+        or not on the class name."""
+        arg = line.split()
+        list_objs = []
+        if not line:
+            for new_obj in models.storage.all().values():
+                list_objs.append(str(new_obj))
+        else:
+            if arg[0] in app_classes:
+                for k, v in models.storage.all().items():
+                    if v.__class__.__name__ == arg[0]:
+                        list_objs.append(str(v))
+            else:
+                print("** class doesn't exist **")
+                return False
+        print(list_objs)
 
     def do_update(self, line):
         """Updates an instance based on the class name and id"""
+        arg = line.split()
+
+        if arg == "":
+            print("** class name missing **")
+        elif arg[0] not in app_classes:
+            print("** class doesn't exist **")
+        elif len(arg) == 1:
+            print("** instance id missing **")
+        elif len(arg) == 2:
+            print("** attribute name missing **")
+        elif len(arg) == 3:
+            print("** value missing **")
+        else:
+            new_obj = "{}.{}".format(arg[0], arg[1])
+            if new_obj not in models.storage.all():
+                print("** no instance found **")
+            else:
+                setattr(models.storage.all()[new_obj], arg[2], arg[3])
+                models.storage.save()
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
