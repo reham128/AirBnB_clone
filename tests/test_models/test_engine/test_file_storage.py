3fe953debb51ce6b3f5dfa898805bd8f16262f9d
Reham <r_nour30@yahoo.com>
2024-02-10 09:15:29 -0800

    up

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Unittests Module to test the class of FleStorage
+"""
+import json
+from models.base_model import BaseModel
+import os
+from models.user import User
+from models.state import State
+from models.place import Place
+from models.city import City
+from models.review import Review
+from models.amenity import Amenity
+from models.engine.file_storage import FileStorage
+from models import storage
+
+
+class TestFileStorage(unittest.TestCase):
+    """class unittest to test FileStorage methods"""
+
+    def setUp(self):
+        """Initialize FileStorage instance"""
+        self.storage = FileStorage()
+        self.new_obj = BaseModel()
+
+        """empty storage befor each test"""
+        self.storage.__objects = {}
+
+    def tearDown(self):
+        """ remove file.json every time after test"""
+        if os.path.exists("file.json"):
+            os.remove("file.json")
+
+    def test_new(self):
+        self.storage.new(self.new_obj)
+        tmp = "{}.{}".format(objec.__class__.__name__, objec.id)
+        self.assertIn(tmp, self.storage.all())
+        self.assertEqual(self.storage._FileStorage__objects[tmp], self.new_obj)
+        
+
+if __name__ == "__main__":
+    unittest.main()
