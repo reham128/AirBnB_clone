658b5a4def96367573682eb70d12516d2d2956cc
Aalaa-Fahim <alaahussin.322001@gmail.com>
2024-02-11 23:15:05 +0200

    Updated

--- models/engine/__init__.py
+++ models/engine/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Creates a unique FIleStorage for the application"""
 from models.engine.file_storage import FileStorage
+import model *
 
 storage = FileStorage()
 storage.reload()
